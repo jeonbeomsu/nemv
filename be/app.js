76cc5d11984e3aa31bb1e17ea275536004142a43
JeonBeomsu <jbs497@naver.com>
2019-12-09 14:47:03 +0900

    git commit

--- be/app.js
+++ be/app.js
@@ -28,6 +28,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+console.log(`${process.env.NODE_ENV} started!`);
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
